cb421fc698083c50894f624c392cfbad9af8b513
Paul Traue <ptrauejr@gmail.com>
2022-01-01 19:32:55 -0600

    Adds some configuration information

---
+++ pysuck/cfg.py
@@ -0,0 +1,8 @@
+class CFG:
+    def __init__(self):
+        self.debug = False
+        self.ssl = False
+        self.pull_server = None
+        self.pull_user = None
+        self.pull_password = None
+        self.db_path = None
