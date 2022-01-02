71b3dab8bb503149a448f0edacdd6973423346c6
Paul Traue <ptrauejr@gmail.com>
2022-01-01 19:33:48 -0600

    Adds command line parsing

--- pysuck.py
+++ pysuck.py
@@ -1,11 +1,32 @@
+#!/usr/bin/env python3
 
-def main(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import argparse
+import signal
+
+import pysuck
+
+def main():
+    """Gets command line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="turn on debugging", action="store_true")
+    parser.add_argument("--ssl", help="Use ssl for pull server", action="store_true")
+    parser.add_argument("--pullserver", help="Server for news pull", default="news")
+    parser.add_argument("--pulluser", help="User for use on the pull server", default=None)
+    parser.add_argument("--pullpassword", help="Password for use on the pull server", default=None)
+    parser.add_argument("--db", help="Path for Database and temporary files", default=None)
+
+    """Parse arguments"""
+    args = parser.parse_args()
+
+    # Initialize app
+    pysuck.initialize(args)
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    main('PyCharm')
+    # Catch signals for quitting properly
+    signal.signal(signal.SIGINT, pysuck.sig_handler)
+    signal.signal(signal.SIGTERM, pysuck.sig_handler)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
